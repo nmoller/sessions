5e3edd9c38af79c7abda880fd16dd2b0449b3e2a
Nelson Moller <moller_clara.nelson@uqam.ca>
2016-08-23 16:38:42 -0400

    Version initial graph nombre de sessions.

---
+++ convert.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nmoller
+ * Date: 23/08/16
+ * Time: 3:56 PM
+ */
+$file = '/home/nmoller/moodle/my_cron/logged_sessions.txt';
+
+$out_file = new SplFileObject("logged_sessions.js", "w");
+$out_file->fwrite("data = [" . PHP_EOL);
+
+
+$in_file = new SplFileObject($file);
+while ($line = $in_file->fgets()) {
+    //2016-08-23 11:36:02	826
+    $content = explode(' ', $line);
+    $content2 = explode("\t", $content[1]);
+
+    $date = explode('-', $content[0]);
+    $hour = explode(':', $content[1]);
+    convert_array_values_to_int($date);
+    convert_array_values_to_int($hour);
+
+
+    @$parsed_line = "{ time : new Date($date[0], $date[1], $date[2], $hour[0], $hour[1]), nbr : $content2[1]},";
+
+    $out_file->fwrite($parsed_line . PHP_EOL);
+
+}
+
+$out_file->fwrite(PHP_EOL. "];");
+
+function convert_array_values_to_int(array &$array) {
+    foreach ($array as $key => $value) {
+        $array[$key] = (int) $value;
+    }
+}
---
+++ logged_sessions.js
@@ -0,0 +1,33 @@
+data = [
+{ time : new Date(2016, 8, 23, 11, 36), nbr : 826
+},
+{ time : new Date(2016, 8, 23, 11, 52), nbr : 846
+},
+{ time : new Date(2016, 8, 23, 12, 7), nbr : 850
+},
+{ time : new Date(2016, 8, 23, 12, 36), nbr : 876
+},
+{ time : new Date(2016, 8, 23, 12, 52), nbr : 869
+},
+{ time : new Date(2016, 8, 23, 13, 7), nbr : 869
+},
+{ time : new Date(2016, 8, 23, 13, 36), nbr : 875
+},
+{ time : new Date(2016, 8, 23, 13, 52), nbr : 847
+},
+{ time : new Date(2016, 8, 23, 14, 7), nbr : 807
+},
+{ time : new Date(2016, 8, 23, 14, 36), nbr : 799
+},
+{ time : new Date(2016, 8, 23, 14, 52), nbr : 791
+},
+{ time : new Date(2016, 8, 23, 15, 7), nbr : 769
+},
+{ time : new Date(2016, 8, 23, 15, 36), nbr : 823
+},
+{ time : new Date(2016, 8, 23, 15, 52), nbr : 816
+},
+{ time : new Date(2016, 8, 23, 16, 7), nbr : 772
+},
+
+];

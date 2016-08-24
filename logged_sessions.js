82b6c49d4d9d9a8762367ad23293d8ba933283aa
Nelson Moller <moller_clara.nelson@uqam.ca>
2016-08-24 07:12:20 -0400

    Version new

--- logged_sessions.js
+++ logged_sessions.js
@@ -33,5 +33,91 @@ data = [
 },
 { time : new Date(2016, 8, 23, 16, 52), nbr : 757
 },
+{ time : new Date(2016, 8, 23, 17, 7), nbr : 728
+},
+{ time : new Date(2016, 8, 23, 17, 36), nbr : 720
+},
+{ time : new Date(2016, 8, 23, 17, 52), nbr : 710
+},
+{ time : new Date(2016, 8, 23, 18, 7), nbr : 656
+},
+{ time : new Date(2016, 8, 23, 18, 36), nbr : 646
+},
+{ time : new Date(2016, 8, 23, 18, 52), nbr : 601
+},
+{ time : new Date(2016, 8, 23, 19, 7), nbr : 547
+},
+{ time : new Date(2016, 8, 23, 19, 36), nbr : 532
+},
+{ time : new Date(2016, 8, 23, 19, 52), nbr : 528
+},
+{ time : new Date(2016, 8, 23, 20, 7), nbr : 500
+},
+{ time : new Date(2016, 8, 23, 20, 36), nbr : 510
+},
+{ time : new Date(2016, 8, 23, 20, 52), nbr : 509
+},
+{ time : new Date(2016, 8, 23, 21, 7), nbr : 477
+},
+{ time : new Date(2016, 8, 23, 21, 36), nbr : 504
+},
+{ time : new Date(2016, 8, 23, 21, 52), nbr : 486
+},
+{ time : new Date(2016, 8, 23, 22, 7), nbr : 477
+},
+{ time : new Date(2016, 8, 23, 22, 36), nbr : 502
+},
+{ time : new Date(2016, 8, 23, 22, 52), nbr : 486
+},
+{ time : new Date(2016, 8, 23, 23, 7), nbr : 447
+},
+{ time : new Date(2016, 8, 23, 23, 36), nbr : 418
+},
+{ time : new Date(2016, 8, 23, 23, 52), nbr : 385
+},
+{ time : new Date(2016, 8, 24, 0, 7), nbr : 349
+},
+{ time : new Date(2016, 8, 24, 0, 36), nbr : 324
+},
+{ time : new Date(2016, 8, 24, 0, 52), nbr : 285
+},
+{ time : new Date(2016, 8, 24, 1, 7), nbr : 253
+},
+{ time : new Date(2016, 8, 24, 1, 36), nbr : 217
+},
+{ time : new Date(2016, 8, 24, 1, 52), nbr : 178
+},
+{ time : new Date(2016, 8, 24, 2, 7), nbr : 155
+},
+{ time : new Date(2016, 8, 24, 2, 36), nbr : 131
+},
+{ time : new Date(2016, 8, 24, 2, 52), nbr : 113
+},
+{ time : new Date(2016, 8, 24, 3, 7), nbr : 96
+},
+{ time : new Date(2016, 8, 24, 3, 36), nbr : 77
+},
+{ time : new Date(2016, 8, 24, 3, 52), nbr : 68
+},
+{ time : new Date(2016, 8, 24, 4, 7), nbr : 55
+},
+{ time : new Date(2016, 8, 24, 4, 36), nbr : 44
+},
+{ time : new Date(2016, 8, 24, 4, 52), nbr : 36
+},
+{ time : new Date(2016, 8, 24, 5, 7), nbr : 31
+},
+{ time : new Date(2016, 8, 24, 5, 36), nbr : 30
+},
+{ time : new Date(2016, 8, 24, 5, 52), nbr : 30
+},
+{ time : new Date(2016, 8, 24, 6, 7), nbr : 31
+},
+{ time : new Date(2016, 8, 24, 6, 36), nbr : 42
+},
+{ time : new Date(2016, 8, 24, 6, 52), nbr : 43
+},
+{ time : new Date(2016, 8, 24, 7, 7), nbr : 52
+},
 
 ];

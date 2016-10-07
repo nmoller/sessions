1f4e55b24c4f667201ecf3698c8622bb060206f1
Nelson Moller <moller_clara.nelson@uqam.ca>
2016-10-07 12:10:04 -0400

    2016-10-07 12:10:04-04:00

---
+++ sessions1.js
@@ -0,0 +1,11 @@
+data1 = [
+{ time : new Date(2016, 9, 7, 11, 7), nbr : 442
+},
+{ time : new Date(2016, 9, 7, 11, 36), nbr : 482
+},
+{ time : new Date(2016, 9, 7, 11, 52), nbr : 475
+},
+{ time : new Date(2016, 9, 7, 12, 7), nbr : 416
+},
+
+];
